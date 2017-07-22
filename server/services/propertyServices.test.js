f4216f1f0daa64d7a1fabbb862fbb255eaa40689
hbtagency <nathan.z@hbtagency.com.au>
2017-07-22 13:23:39 +1000

    unit test added against services

--- server/services/propertyServices.test.js
+++ server/services/propertyServices.test.js
@@ -1,1 +1,20 @@
 //Unit Test is going along with services
+var propertyServices = require('./propertyServices');
+var expect = require('chai').expect;
+
+describe('Property Services', () => {
+  it('should be able to list all available properties', () => {
+      propertyServices.getPropertyList()
+      .then(properties => {
+        expect(properties).to.be.a('array');
+        expect(properties).to.have.lengthOf.above(1);
+      })
+  });
+
+//   it('should be able add a property', () => {
+//   });
+
+//   it('should be able add remove a property', () => {
+//   });
+
+});
